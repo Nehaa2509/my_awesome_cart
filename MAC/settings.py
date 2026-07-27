ed749714bc1f3bdc1b42bf292376074e087676ed
Nehaa2509 <221210116040@aiet.edu.in>
2026-07-28 00:44:00 +0530

    Configure production deployment settings, Procfile, Gunicorn, and updated UI/blog components

--- MAC/settings.py
+++ MAC/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Managing Media (Images, documents uploaded by users) Managging Media
 
