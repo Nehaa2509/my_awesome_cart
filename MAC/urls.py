df64ec08268af4d79441207336fd2d4f3b334fd5
Nehaa2509 <221210116040@aiet.edu.in>
2026-07-28 19:18:02 +0530

    fix: track product and blog images in git and enable media file serving in production

--- MAC/urls.py
+++ MAC/urls.py
@@ -15,9 +15,14 @@ admin.site.site_header = "My Awesome Cart Admin"
 admin.site.site_title = "MyAwesomeCart Admin"
 admin.site.index_title = "Dashboard"
 
+from django.views.static import serve
+from django.urls import re_path
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
     path('blog/', include('blog.urls')),
     path('', shop_views.index, name='Home'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
